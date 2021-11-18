955bb4dd9dbb03a92873f5101e11889e689df03a
codebayowl <codebayowl@gmail.com>
2021-11-19 00:13:43 +0200

    creating basic app structure

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,35 @@
-console.log("Connected");
+var app = new Vue({
+    el: '#canvas',
+    data: {
+        newContent: "",
+        unchecked: [],
+        checked: []
+    },
+    methods: {
+        addItem() {
+            if (this.newContent) {
+				this.unchecked.push({
+                    id: newid(), 
+                    done: false, 
+                    content: this.newContent
+                });
+				this.newContent = "";
+			}
+        },
+
+        delItem() {
+            //
+        },
+
+        checkItem() {
+            //
+        },
+
+        unchkItem() {
+            //
+        }
+    },
+    computed: {
+        //
+    }
+})
